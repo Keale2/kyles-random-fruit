9b6cf398ebeac6c16a17813cc1302e8c4e6ce8a4
Kyle Wagner <Keale2@gmail.com>
2016-02-23 15:48:41 -0500

    chore: Add Travis config, conventional commit, and semantic release

---
+++ src/index.test.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect;
+var kylesRandomFruit = require('./index');
+
+describe('kyles-random-fruit', function () {
+    describe('all', function () {
+       it('should be an array of strings', function () {
+           expect(kylesRandomFruit.all).to.satisfy(isArrayOfStrings);
+           
+           function isArrayOfStrings(arr) {
+               return arr.every(function (item) {
+                   return typeof item === "string";
+               });
+           }
+       });
+    });
+    
+    describe('random', function () {
+        it('should return a random item from kylesRandomFruit.all', function () {
+            var randomItem = kylesRandomFruit.random();
+            expect(kylesRandomFruit.all).to.include(randomItem);
+        });
+    })
+    
+});
